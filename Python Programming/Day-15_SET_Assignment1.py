96553ef63f55d82f6dfbcb4156998ade0a1b4749
Revansh Bugade <bugader3857@gmail.com>
2024-09-17 19:39:06 +0530

    Update Day-15_SET_Assignment1.py

--- Python Programming/Day-15_SET_Assignment1.py
+++ Python Programming/Day-15_SET_Assignment1.py
@@ -1,4 +1,8 @@
+" 1. Write a Python program to Get Only unique items from two sets. "
 set1 = {10, 20, 30, 40, 50}
 set2 = {40, 50, 60, 70,80}
 set = set1.union(set2)
 print(set)
+
+#output:
+{70, 40, 10, 50, 20, 60, 30} 
