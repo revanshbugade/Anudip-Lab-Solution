4eed342d2e61005c14912e62a4f0eb77fae21a29
Revansh Bugade <bugader3857@gmail.com>
2024-10-01 12:48:06 +0530

    Add files via upload

---
+++ Python Programming/Day-25 Pandas_Assignment2.py
@@ -0,0 +1,28 @@
+"""2: Suppose you want to track and analyze your household expenses for a month. You have recorded the expenses for various categories, such as groceries, utilities, rent, transportation, and entertainment. You can represent this expense data using a Pandas Series. 
+Input: # Expense categories
+ categories = ['Groceries', 'Utilities', 'Rent', 'Transportation', 'Entertainment']
+# Monthly expense data (example data in USD) 
+expenses = [500, 200, 1200, 300, 150]"""
+
+import pandas as pd
+
+# Expense categories
+categories = ['Groceries', 'Utilities', 'Rent', 'Transportation', 'Entertainment']
+
+# Monthly expense data (example data in USD)
+expenses = [500, 200, 1200, 300, 150]
+
+# Create a Pandas Series
+expenses_series = pd.Series(expenses, index=categories)
+
+# Display the Series
+print(expenses_series)
+
+#output:
+
+Groceries          500
+Utilities          200
+Rent              1200
+Transportation     300
+Entertainment      150
+dtype: int64
